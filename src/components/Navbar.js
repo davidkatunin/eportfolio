d28b1b2b6ce143f929caa855d09fa64c403ef6ff
davidkatunin <davidkatunin@gmail.com>
2025-07-19 13:19:49 +0300

    deleting react js approach

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-function Navbar({ show }) {
-  return (
-    <div className={`fixed top-0 left-0 w-full transition-transform duration-500 ease-in-out z-50 ${show ? 'translate-y-0' : '-translate-y-full'}`}>
-      <h1 className="text-4xl text-[#9e998d] pt-3 pb-3 pl-8 shadow-md" style={{ backgroundColor: '#080f1d' }}> David Katunin </h1>
-    </div>
-  );
-}
-
-export default Navbar;
